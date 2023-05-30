680402a82ec3cddcaec687481c0dc08224cc3a0a
Irfan-Siddiqui-au52 <siddiquiirfan234@gmail.com>
2023-05-30 16:19:19 +0530

    Uploading my attainUbackend Project

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ const server = app.listen(PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://main--fanciful-crepe-b79bd4.netlify.app/",
     credentials: true,
   },
 });
